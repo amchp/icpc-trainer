cc6022dc87900a7736287bdfe98a705dc8ef266f
Alejandro Mc Ewen <mcewenalejandro@gmail.com>
2026-06-26 16:20:39 -0500

    Allow domains

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
     }
   },
   plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: ["icpc-trainer.site"]
+  },
   server: {
     host: "127.0.0.1",
     port: 5173,
